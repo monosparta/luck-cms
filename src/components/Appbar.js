2c78a38b67cf0bfa64d28ee76f8806174a88e4cd
Hsieh <chian0221@gmail.com>
2022-04-13 11:03:49 +0800

    Appbar logout

--- src/components/Appbar.js
+++ src/components/Appbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
+import Button from '@mui/material/Button';
 import Toolbar from "@mui/material/Toolbar";
 import Avatar from "@mui/material/Avatar";
 import LogoutIcon from "@mui/icons-material/Logout";
@@ -34,19 +35,21 @@ const Appbar = () => {
 
 	return (
 		<Box sx={{ flexGrow: 1 }}>
-			<AppBar position='static'>
-				<Toolbar>
+			<AppBar position='static' >
+				<Toolbar backgroundColor='#BCD5DE'>
 					<img src='./mono.png' alt='' className='logo' />
 					<p className='title'>會員置物櫃管理系統</p>
 					<Box sx={{ flexGrow: 1 }} />
 					<div className='Bar'>
-						<div className='Bar-item'>
+						<div className='Bar-user'>
 							<Avatar>R</Avatar>
 							<p className='username'> Rosa </p>
 						</div>
-						<div className='Bar-item'>
+						<div className='Bar-logout'>
 							<LogoutIcon></LogoutIcon>
-							<p className='Logout'> Logout</p>
+              <stack>
+							<Button variant="Logout" className='Bar-logout' style={{textTransform: 'none'}}> Logout </Button>
+              </stack>
 						</div>
 					</div>
 				</Toolbar>
