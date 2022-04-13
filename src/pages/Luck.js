fe29e15b65bc4e6da59bf1ef64960c7912411a4a
leonnn124 <s0976144676s@gmail.com>
2022-04-13 11:20:35 +0800

    luck page slice done

---
+++ src/pages/Luck.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Luck.css";
+
+const Luck = () => {
+  return (
+    <div id="Luck">
+      <div className="luck__title"></div>
+      <div className="luck__toggle"></div>
+    </div>
+  );
+};
+
+export default Luck;
