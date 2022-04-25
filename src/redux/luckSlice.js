63f0e21770e685517d85525574de111d86adc2cb
Hsieh <chian0221@gmail.com>
2022-04-25 14:05:37 +0800

    add state icon

--- src/redux/luckSlice.js
+++ src/redux/luckSlice.js
@@ -1,21 +1,6 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 export const LuckStatus = createAsyncThunk(
-<<<<<<< HEAD
-  "Luck/LuckStatus",
-  async (thunkAPI) => {
-    try {
-      const token = localStorage.getItem("token");
-      const response = await fetch(
-        "https://dd82-211-72-239-241.ngrok.io/api/locker",
-        {
-          method: "GET",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-            token,
-          },
-=======
     "Luck/LuckStatus",
     async (thunkAPI) => {
         try {
@@ -41,67 +26,53 @@ export const LuckStatus = createAsyncThunk(
         } catch (e) {
             console.log(thunkAPI.rejectWithValue(e));
             return thunkAPI.rejectWithValue(e);
->>>>>>> d39aec9b9073af7dbaaa874c9cdde491f72bbe5d
         }
-      );
-      let data = await response.json();
-      console.log("response", data);
-      if (response.status === 200) {
-        return data;
-      } else {
-        throw data.message;
-      }
-    } catch (e) {
-      console.log(thunkAPI.rejectWithValue(e));
-      return thunkAPI.rejectWithValue(e);
-    }
-  }
-);
+    })
 
 export const luckSlice = createSlice({
-  name: "Luck",
-  initialState: {
-    Lock: [{}],
-    currentNumber: "",
-    isFetching: false,
-    isSuccess: false,
-    isError: false,
-    errorMessage: "",
-  },
-  reducers: {
-    clearState: (state) => {
-      state.Lock = [{}];
-      state.isError = false;
-      state.isSuccess = false;
-      state.isFetching = false;
+    name: "Luck",
+    initialState: {
+        Lock: [{}],
+        currentNumber: "",
+        isFetching: false,
+        isSuccess: false,
+        isError: false,
+        errorMessage: "",
+    },
+    reducers: {
+        clearState: (state) => {
+            state.Lock = [{}];
+            state.isError = false;
+            state.isSuccess = false;
+            state.isFetching = false;
 
-      return state;
-    },
-    // saveLock: (state, { payload }) => {
+            return state;
+        },
+        // saveLock: (state, { payload }) => {
 
-    // }
-  },
-  extraReducers: {
-    [LuckStatus.fulfilled]: (state, { payload }) => {
-      console.log("Good", payload);
-      state.isFetching = false;
-      state.isSuccess = true;
-      state.Lock = payload;
-      return state;
+        // }
     },
-    [LuckStatus.pending]: (state) => {
-      state.isFetching = true;
-      console.log("loading");
-      return state;
+    extraReducers: {
+        [LuckStatus.fulfilled]: (state, { payload }) => {
+            console.log("Good", payload);
+            state.isFetching = false;
+            state.isSuccess = true;
+            state.Lock = payload;
+            return state;
+        },
+        [LuckStatus.pending]: (state) => {
+            state.isFetching = true;
+            console.log("loading");
+            return state;
+        },
+        [LuckStatus.rejected]: (state, { payload }) => {
+            console.log("Bad", payload);
+            state.isFetching = false;
+            state.isError = true;
+            state.errorMessage = payload.message;
+            return state;
+        },
     },
-    [LuckStatus.rejected]: (state, { payload }) => {
-      console.log("Bad", payload);
-      state.isFetching = false;
-      state.isError = true;
-      state.errorMessage = payload.message;
-      return state;
-    },
-  },
 });
 
 export const { clearState } = luckSlice.actions;
