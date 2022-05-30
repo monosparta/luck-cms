ddb16a23ea7cbd66e84a1d3857a33607273f8f63
leonnn124 <50895365+leonnn124@users.noreply.github.com>
2022-05-30 11:14:03 +0800

    Update App.test.js
    modify app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import store from "./store/index";
 
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
 test("renders learn react link", () => {
-  render(
+  root.render(
     <Provider store={store}>
       <App />
     </Provider>
