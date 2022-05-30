24ff995a930abebd97a6393e04cfaf0b468fb401
jonathan <48669926+rj1110634028@users.noreply.github.com>
2022-05-30 11:45:37 +0800

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "./store/index";
 
-test('renders luck link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/立即登入/i);
+test("renders learn react link", () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
