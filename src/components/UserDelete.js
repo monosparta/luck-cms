9fce8204b26a0930dfbdf54c0ead5395f5e83cf3
leonnn124 <s0976144676s@gmail.com>
2022-06-16 14:06:59 +0800

    delete user

---
+++ src/components/UserDelete.js
@@ -0,0 +1,142 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { userDelete } from "../redux/userSlice";
+import { selectUser } from "../redux/userSlice";
+import ClearAllIcon from "@mui/icons-material/ClearAll";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogTitle from "@mui/material/DialogTitle";
+import Collapse from "@mui/material/Collapse";
+import Alert from "@mui/material/Alert";
+import Stack from "@mui/material/Stack";
+
+const UserDelete = (props) => {
+  const dispatch = useDispatch();
+
+  const [checkOpen, setCheckOpen] = React.useState(false);
+  const [alertOpen, setAlertOpen] = React.useState(false);
+  const { userClear } = useSelector(selectUser);
+
+  const handleUserDelete = () => {
+    dispatch(userDelete({ id: props.user.id }));
+    setCheckOpen(false);
+    setAlertOpen(true);
+    setTimeout(() => {
+      setAlertOpen(false);
+    }, 3000);
+  };
+
+  if (userClear) {
+    props.handleClickRefresh();
+  }
+  const handleCheckOpen = () => {
+    setCheckOpen(true);
+  };
+  const handleCheckClose = () => {
+    setCheckOpen(false);
+  };
+  return (
+    <div className="userDelete">
+      <ClearAllIcon
+        onClick={handleCheckOpen}
+        sx={{
+          color: "gray",
+          height: "20px",
+          width: "20px",
+          cursor: "pointer",
+        }}
+      />
+      <Dialog
+        open={checkOpen}
+        onClose={handleCheckClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          m: "auto",
+          borderRadius: "10px",
+          "& .MuiDialog-container": {
+            "& .MuiPaper-root": {
+              width: 375,
+              height: 250, // Set your width here
+              borderRadius: "10px",
+            },
+            "& .MuiOutlinedInput-root": {
+              width: 244, // Set your width here
+              height: 150,
+            },
+            "& .MuiDialogContent-root ": {
+              padding: 0,
+            },
+            "& .MuiDialogActions-root ": {
+              margin: "0 auto",
+            },
+          },
+        }}
+      >
+        <DialogTitle
+          id="alert-dialog-title"
+          sx={{ textAlign: "center", padding: "16px 24px 0 24px" }}
+        >
+          <div className="alert">
+            <img src="./alert.png" alt="" className="alert" />
+            <p>確定要執行刪除會員資料的動作嗎？</p>
+          </div>
+        </DialogTitle>
+        <DialogActions sx={{ width: 244 }}>
+          <Button
+            variant="contained"
+            onClick={handleUserDelete}
+            style={{
+              width: 108,
+              height: 36,
+              background: "#2F384F",
+              boxShadow: "none",
+              fontSize: 12,
+              margin: 5,
+            }}
+          >
+            確認
+          </Button>
+          <Button
+            variant="contained"
+            onClick={handleCheckClose}
+            style={{
+              width: 108,
+              height: 36,
+              background: "#fff",
+              color: "#2F384F",
+              boxShadow: "none",
+              fontSize: 12,
+              margin: 5,
+              border: "1px solid #2F384F",
+            }}
+          >
+            取消
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <Stack
+        className="success"
+        sx={{
+          width: "478px",
+          height: "52px",
+          top: "107px",
+          position: "absolute",
+          right: "24px",
+        }}
+        spacing={2}
+      >
+        <Collapse in={alertOpen}>
+          <Alert variant="filled" severity="error">
+            已刪除該會員資料
+          </Alert>
+        </Collapse>
+      </Stack>
+    </div>
+  );
+};
+
+export default UserDelete;
--- src/components/UserInfoTitle.js
+++ src/components/UserInfoTitle.js
@@ -3,6 +3,7 @@ import { useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { selectUser } from "../redux/userSlice";
 
+import UserDelete from "../components/UserDelete";
 import LockOpenIcon from "@mui/icons-material/LockOpen";
 import LockIcon from "@mui/icons-material/Lock";
 import Skeleton from "@mui/material/Skeleton";
@@ -19,6 +20,12 @@ const UserInfoTitle = (props) => {
         <>
           {props.luckIconStatus === 1 ? <LockIcon /> : <LockOpenIcon />}
           <h1>置物櫃 - {location.state}</h1>
+
+          <UserDelete
+            setUserStatus={props.setUserStatus}
+            user={props.user}
+            handleClickRefresh={props.handleClickRefresh}
+          />
         </>
       )}
     </div>
